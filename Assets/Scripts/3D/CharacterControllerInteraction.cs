7be2efa522aef674b4a9b6b0a6fabb2a209d1bef
Lilian Barbier <lilian.barbierlaffitte@gmail.com>
2024-03-06 23:33:11 +0100

    added animation when opening door

--- Assets/Scripts/3D/CharacterControllerInteraction.cs
+++ Assets/Scripts/3D/CharacterControllerInteraction.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
@@ -8,7 +9,7 @@ using UnityEngine.Animations.Rigging;
 public class CharacterControllerInteraction : MonoBehaviour
 {
     private InputManager inputManager;
-    private Transform cameraTransform; 
+    private Transform cameraTransform;
 
     [SerializeField] private float interactionDistance = 2.0f;
 
@@ -27,7 +28,7 @@ public class CharacterControllerInteraction : MonoBehaviour
         inputManager = InputManager.Instance;
         inputManager.playerInteractEvent += Interact;
         cameraTransform = Camera.main.transform;
-        rightArmRig =  GetComponentInChildren<TwoBoneIKConstraint>();
+        rightArmRig = GetComponentInChildren<TwoBoneIKConstraint>();
         armTargetPoint = rightArmRig.transform.GetChild(0);
 
         rightArmRig.weight = 0;
@@ -35,51 +36,83 @@ public class CharacterControllerInteraction : MonoBehaviour
 
     private void Interact()
     {
-        if(inputManager.interactionState == InputManager.InteractionState.OpenDoor)
+        if (inputManager.interactionState == InputManager.InteractionState.OpenDoor)
         {
             FinishInteraction();
         }
-        else{
+        else
+        {
             Debug.DrawRay(cameraTransform.position, cameraTransform.forward * interactionDistance, Color.red, 2);
 
             // Does the ray intersect any objects 
             if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out RaycastHit hit, interactionDistance))
             {
-                if(hit.transform.CompareTag("Door")){
+                if (hit.transform.CompareTag("Door"))
+                {
                     currentDoor = hit.transform.GetComponent<Door>();
                     inputManager.interactionState = InputManager.InteractionState.OpenDoor;
-                    //todo : a modifier
-                    newArmTargetPoint = hit.transform.GetChild(0).position;
+                    newArmTargetPoint = currentDoor.handlePosition.position;
                     rightArmRig.weight = 1;
+                    transform.position = currentDoor.FrontPlayerPosition.position;
+                    currentDoor.doorCameraAnimation.Priority = 20;
                 }
             }
         }
     }
 
-    private void FinishInteraction(){
+    private void FinishInteraction()
+    {
         rightArmRig.weight = 0;
+
+        //Vérifier si possible de faire plus propre, le CharacterController empéche de changer la position directement
+        var charController = GetComponent<CharacterController>();
+        charController.enabled = false;
+        transform.position = new Vector3(cameraTransform.position.x, transform.position.y, cameraTransform.position.z);
+        charController.enabled = true;
+
+        currentDoor.doorCameraAnimation.Priority = 0;
         inputManager.interactionState = InputManager.InteractionState.Nothing;
     }
 
-    void Update(){
-
-        if(newArmTargetPoint != null){
-            armTargetPoint.position = Vector3.Lerp(armTargetPoint.position, newArmTargetPoint, Time.deltaTime * 5);
+    void Update()
+    {
+        if (newArmTargetPoint != null)
+        {
+            newArmTargetPoint = currentDoor.handlePosition.position;
+            armTargetPoint.position = newArmTargetPoint;
         }
 
-        if(inputManager.interactionState == InputManager.InteractionState.OpenDoor){
+        if (inputManager.interactionState == InputManager.InteractionState.OpenDoor)
+        {
+            //voir pour passer plutot par un évenement pour éviter les moment ou on garde la touche appuyer
+            if (inputManager.GetMovement() != Vector2.zero)
+                FinishInteraction();
+
             //Get inverse mouse delta to open the door
             float angleDelta = -inputManager.GetMouseDelta().x * doorOpeningSpeed * Time.deltaTime;
+            transform.position = currentDoor.FrontPlayerPosition.position;
 
-            if(currentDoor.currentDoorAngle + angleDelta < currentDoor.maxDoorAngle && currentDoor.currentDoorAngle + angleDelta > currentDoor.doorCloseAngle)
+            //update camera dolly position to match the door opening
+            currentDoor.doorCameraAnimation.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition = currentDoor.currentDoorAngle / currentDoor.maxDoorAngle;
+
+            if (currentDoor.currentDoorAngle + angleDelta < currentDoor.maxDoorAngle && currentDoor.currentDoorAngle + angleDelta > currentDoor.doorCloseAngle)
                 currentDoor.currentDoorAngle += angleDelta;
 
             //If the door is open to 85% of maxAngle, we open it completely and stop the interaction
-            if(currentDoor.currentDoorAngle > currentDoor.maxDoorAngle * 0.85f){
+            if (currentDoor.currentDoorAngle > currentDoor.maxDoorAngle * 0.85f && !currentDoor.isOpen)
+            {
                 currentDoor.currentDoorAngle = currentDoor.maxDoorAngle;
+                currentDoor.isOpen = true;
                 FinishInteraction();
             }
-                
+            //If the door is closed to 5% of maxAngle, we close it completely and stop the interaction
+            else if (currentDoor.currentDoorAngle < currentDoor.maxDoorAngle * 0.1f && currentDoor.isOpen)
+            {
+                currentDoor.currentDoorAngle = currentDoor.doorCloseAngle;
+                currentDoor.isOpen = false;
+                FinishInteraction();
+            }
+
         }
     }
 
--- Assets/Scripts/Objects/Door.cs
+++ Assets/Scripts/Objects/Door.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] public float maxDoorAngle = 90.0f;
-    [SerializeField] public float doorCloseAngle = 0.0f;
+    public bool isOpen = false;
+    public float maxDoorAngle = 90.0f;
+    public float doorCloseAngle = 0.0f;
     [Range(0.0f, 360.0f)] public float currentDoorAngle = 0.0f;
+    public CinemachineVirtualCamera doorCameraAnimation;
+
+    public Transform handlePosition;
+    public Transform FrontPlayerPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +23,14 @@ public class Door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(currentDoorAngle < maxDoorAngle){
+        if (currentDoorAngle < maxDoorAngle)
+        {
             transform.rotation = Quaternion.Euler(0, currentDoorAngle, 0);
         }
-        else{
+        else
+        {
             transform.rotation = Quaternion.Euler(0, maxDoorAngle, 0);
         }
-        
+
     }
 }
